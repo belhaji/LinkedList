55d5c1d21b0fc48ae712f5eb0d30201ead083f5d
Adil Belhaji <belhaji.dev@gmail.com>
2016-03-08 20:42:31 +0000

    Initial Commit

---
+++ S2Project/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  S2Project
+//
+//  Created by Adil on 3/8/16.
+//  Copyright © 2016 Adil. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
